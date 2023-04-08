afacb7aa2c714dcce12b33fbf373701589a609ce
Bartek Łukawski <bwmn.peter@gmail.com>
2023-04-08 17:37:01 +0000

    Start SensorInterruptHandler

--- main.cpp
+++ main.cpp
@@ -5,10 +5,56 @@
 
 #include "mbed.h"
 
+class SensorInterruptHandler
+{
+public:
+    SensorInterruptHandler(PinName clockPin, PinName dataPin, EventFlags & _flags)
+        : interrupt(clockPin),
+          data(dataPin),
+          flags(_flags),
+          index(0)
+    {
+        interrupt.rise(callback(this, &SensorInterruptHandler::accept));
+    }
+
+    static const int HIGHEST_INDEX = 20;
+
+private:
+    void accept()
+    {
+        unsigned int value = data.read();
+
+        if (value)
+        {
+            flags.set(1 << index);
+        }
+
+        if (++index == HIGHEST_INDEX)
+        {
+            // signal the blocked thread to resume execution
+            flags.set(1 << HIGHEST_INDEX);
+            index = 0;
+        }
+    }
+
+    InterruptIn interrupt;
+    DigitalIn data;
+    EventFlags & flags;
+    volatile int index;
+};
+
 int main()
 {
+    EventFlags sensorFlags;
+    SensorInterruptHandler sensorInterrupt(p10, p9, sensorFlags);
+
     while (true)
     {
-        ;
+        uint32_t raw = sensorFlags.wait_all(1 << SensorInterruptHandler::HIGHEST_INDEX);
+
+        if (raw >> 31 == 0) // no error, see osFlagsError otherwise
+        {
+            ;
+        }
     }
 }
