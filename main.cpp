fd2c0a5898080dae7ae3da4bf0590f7fde9c41a4
Bartek Łukawski <bwmn.peter@gmail.com>
2023-04-09 22:20:19 +0000

    Use thread flags to signal readiness

--- main.cpp
+++ main.cpp
@@ -11,39 +11,37 @@ PinName sensorDataPin = p10;
 class SensorInterruptHandler
 {
 public:
-    SensorInterruptHandler(PinName clockPin, PinName dataPin, EventFlags & _flags)
+    SensorInterruptHandler(PinName clockPin, PinName dataPin, Thread & _thread)
         : interrupt(clockPin),
           data(dataPin),
-          flags(_flags),
-          index(HIGHEST_INDEX)
+          thread(_thread),
+          index(FRAME_LENGTH),
+          buffer(SIGNAL_BIT)
     {
-        interrupt.rise(callback(this, &SensorInterruptHandler::accept));
+        interrupt.rise(callback(this, &SensorInterruptHandler::onClockRisingEdge));
     }
 
-    static const int HIGHEST_INDEX = 20;
+    static const unsigned FRAME_LENGTH = 20;
+    static const uint32_t SIGNAL_BIT = 0x10000000U; // don't use 0x80000000U as it is `osFlagsError`
 
 private:
-    void accept()
+    void onClockRisingEdge()
     {
-        if (data)
-        {
-            flags.set(1UL << index);
-        }
+        buffer |= data << --index;
 
         if (index == 0)
         {
-            // signal the blocked thread to resume execution
-            flags.set(1UL << HIGHEST_INDEX);
-            index = HIGHEST_INDEX;
+            thread.flags_set(buffer);
+            index = FRAME_LENGTH;
+            buffer = SIGNAL_BIT;
         }
-
-        index--;
     }
 
     InterruptIn interrupt;
     DigitalIn data;
-    EventFlags & flags;
+    Thread & thread;
     volatile int index;
+    volatile uint32_t buffer;
 };
 
 enum SensorChannel : uint8_t
@@ -54,54 +52,61 @@ enum SensorChannel : uint8_t
     CALIBRATION
 };
 
-int main()
+void sensorWorker(SensorInterruptHandler * handler)
 {
-    EventFlags sensorFlags;
-    SensorInterruptHandler sensorInterrupt(sensorClockPin, sensorDataPin, sensorFlags);
-
     uint32_t raw;
 
     while (true)
     {
-        raw = sensorFlags.wait_all(1UL << SensorInterruptHandler::HIGHEST_INDEX);
+        raw = ThisThread::flags_wait_all(SensorInterruptHandler::SIGNAL_BIT) & 0x000FFFFF; // 20 bits
 
-        if (raw >> 31 == 0) // no error, see osFlagsError otherwise
+        uint8_t rawChannel = raw >> 16; // 4 MSB
+        uint16_t rawData = raw & 0x0000FFFF; // two's complement representation of a signed 16-bit number
+
+        int16_t data = static_cast<int16_t>(rawData);
+
+        switch (rawChannel)
         {
-            raw &= 0x000FFFFF; // 20 bits
-            uint8_t rawChannel = raw >> 16; // 4 MSB
-            uint16_t rawData = raw & 0x0000FFFF; // two's complement representation of a signed 16-bit number
-            int16_t data = static_cast<int16_t>(rawData);
-
-            switch (rawChannel)
-            {
-            case VOLTAGE_LEVEL:
-                printf("Voltage level: %d\n", data);
-                break;
-            case FX:
-                printf("Fx: %d\n", data);
-                break;
-            case FY:
-                printf("Fy: %d\n", data);
-                break;
-            case FZ:
-                printf("Fz: %d\n", data);
-                break;
-            case MX:
-                printf("Mx: %d\n", data);
-                break;
-            case MY:
-                printf("My: %d\n", data);
-                break;
-            case MZ:
-                printf("Mz: %d\n", data);
-                break;
-            case CALIBRATION:
-                printf("Calibration: %d\n", data);
-                break;
-            default:
-                printf("Unknown channel: %d\n", data);
-                break;
-            }
+        case VOLTAGE_LEVEL:
+            printf("Voltage level: %d\n", data);
+            break;
+        case FX:
+            printf("Fx: %d\n", data);
+            break;
+        case FY:
+            printf("Fy: %d\n", data);
+            break;
+        case FZ:
+            printf("Fz: %d\n", data);
+            break;
+        case MX:
+            printf("Mx: %d\n", data);
+            break;
+        case MY:
+            printf("My: %d\n", data);
+            break;
+        case MZ:
+            printf("Mz: %d\n", data);
+            break;
+        case CALIBRATION:
+            printf("Calibration: %d\n", data);
+            break;
+        default:
+            printf("Unknown channel: %d\n", data);
+            break;
         }
     }
+}
+
+int main()
+{
+    Thread sensorThread;
+    SensorInterruptHandler sensorInterrupt(sensorClockPin, sensorDataPin, sensorThread);
+
+    sensorThread.start(callback(sensorWorker, &sensorInterrupt));
+
+    while (true)
+    {}
+
+    sensorThread.join();
 }
