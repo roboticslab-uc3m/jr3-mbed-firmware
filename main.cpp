1fd91c3ee2eae3c8dd69cd37e6991584df95ffe8
Bartek Łukawski <bwmn.peter@gmail.com>
2023-06-12 19:27:32 +0200

    [TEST] Watch clock edges

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
  **/
 
 #include "mbed.h"
+#include "LPC17xx.h"
 
 PinName sensorClockPin = p9;
 PinName sensorDataPin = p10;
+
+// DigitalOut led(LED1);
 
 class SensorInterruptHandler
 {
@@ -14,13 +17,19 @@ public:
     SensorInterruptHandler(PinName clockPin, PinName dataPin, Thread & _thread)
         : clock(clockPin),
           data(dataPin),
+          clockIn(clockPin),
+          dataIn(dataPin),
           thread(_thread),
           isTransmittingFrame(false),
           isStartPulse(false),
+          clockState(true),
+          dataState(true),
           index(FRAME_LENGTH),
           buffer(SIGNAL_BIT)
     {
+        clock.fall(callback(this, &SensorInterruptHandler::onClockFallingEdge));
         clock.rise(callback(this, &SensorInterruptHandler::onClockRisingEdge));
+
         data.fall(callback(this, &SensorInterruptHandler::onDataFallingEdge));
         data.rise(callback(this, &SensorInterruptHandler::onDataRisingEdge));
     }
@@ -29,17 +38,34 @@ public:
     static const uint32_t SIGNAL_BIT = 0x10000000U; // don't use 0x80000000U as it is `osFlagsError`
 
 private:
+    void onClockFallingEdge()
+    {
+        clockState = false;
+    }
+
     void onClockRisingEdge()
     {
+        clockState = true;
+
         if (isTransmittingFrame)
         {
-            buffer |= data << --index;
+            index--;
+
+            if (!dataState)
+            {
+                buffer = buffer | (1UL << index);
+            }
+            else
+            {
+                buffer = buffer | ((index % 2 == 0) << index);
+            }
 
             if (index == 0)
             {
                 isTransmittingFrame = false;
                 thread.flags_set(buffer);
                 index = FRAME_LENGTH;
+                // led = 0;
                 buffer = SIGNAL_BIT;
             }
         }
@@ -52,7 +78,9 @@ private:
 
     void onDataFallingEdge()
     {
-        if (!isTransmittingFrame && clock == 1)
+        dataState = false;
+
+        if (!isTransmittingFrame && clockState)
         {
             isStartPulse = true;
         }
@@ -60,19 +88,36 @@ private:
 
     void onDataRisingEdge()
     {
-        if (!isTransmittingFrame && isStartPulse && clock == 1)
+        dataState = true;
+
+        if (!isTransmittingFrame && isStartPulse && clockState)
         {
             isStartPulse = false;
             isTransmittingFrame = true;
+            // led = 1;
+            // buffer = SIGNAL_BIT;
         }
+
+        // buffer |= (dataState ? 1 : 0) << --index;
+
+        // if (index == 0)
+        // {
+        //     thread.flags_set(buffer);
+        //     index = FRAME_LENGTH;
+        //     buffer = SIGNAL_BIT;
+        // }
     }
 
     InterruptIn clock;
     InterruptIn data;
+    DigitalIn clockIn;
+    DigitalIn dataIn;
     Thread & thread;
 
     volatile bool isTransmittingFrame;
     volatile bool isStartPulse; // pulse high-low-high on DATA while DLCK is high
+    volatile bool clockState;
+    volatile bool dataState;
     volatile int index;
     volatile uint32_t buffer;
 };
@@ -98,36 +143,38 @@ void sensorWorker(SensorInterruptHandler * handler)
 
         int16_t data = static_cast<int16_t>(rawData);
 
-        switch (rawChannel)
-        {
-        case VOLTAGE_LEVEL:
-            printf("Voltage level: %d\n", data);
-            break;
-        case FX:
-            printf("Fx: %d\n", data);
-            break;
-        case FY:
-            printf("Fy: %d\n", data);
-            break;
-        case FZ:
-            printf("Fz: %d\n", data);
-            break;
-        case MX:
-            printf("Mx: %d\n", data);
-            break;
-        case MY:
-            printf("My: %d\n", data);
-            break;
-        case MZ:
-            printf("Mz: %d\n", data);
-            break;
-        case CALIBRATION:
-            printf("Calibration: %d\n", data);
-            break;
-        default:
-            printf("Unknown channel: %d\n", data);
-            break;
-        }
+        printf("0x%08x 0x%08x 0x%02x\n", rawData, data, rawChannel);
+
+        // switch (rawChannel)
+        // {
+        // case VOLTAGE_LEVEL:
+        //     printf("Voltage level: %d\n", data);
+        //     break;
+        // case FX:
+        //     printf("Fx: %d\n", data);
+        //     break;
+        // case FY:
+        //     printf("Fy: %d\n", data);
+        //     break;
+        // case FZ:
+        //     printf("Fz: %d\n", data);
+        //     break;
+        // case MX:
+        //     printf("Mx: %d\n", data);
+        //     break;
+        // case MY:
+        //     printf("My: %d\n", data);
+        //     break;
+        // case MZ:
+        //     printf("Mz: %d\n", data);
+        //     break;
+        // case CALIBRATION:
+        //     printf("Calibration: %d\n", data);
+        //     break;
+        // default:
+        //     printf("Unknown channel: %d\n", data);
+        //     break;
+        // }
     }
 }
 
@@ -139,7 +186,10 @@ int main()
     sensorThread.start(callback(sensorWorker, &sensorInterrupt));
 
     while (true)
-    {}
+    {
+        printf("loop\n");
+        ThisThread::sleep_for(1000);
+    }
 
     sensorThread.join();
 }
