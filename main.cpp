3432146613bc0fd76a5116b90bafd0ed3380aaba
Bartek Łukawski <bwmn.peter@gmail.com>
2023-07-16 21:05:28 +0200

    Remove temporary code, clean files

--- main.cpp
+++ main.cpp
@@ -1,60 +1,15 @@
 #include "mbed.h"
-#include "JR3.hpp"
-#include "JR3Port.hpp"
-#include "JR3PortRegister.hpp"
+#include "Jr3.hpp"
 
 #define CLOCK_PIN p9
 #define DATA_PIN p10
 
-void setSystemFrequency(int clkDiv, int M, int N)
-{
-    // see M/N values at https://os.mbed.com/users/no2chem/notebook/mbed-clock-control--benchmarks/
-    // this code was mostly borrowed from https://os.mbed.com/forum/mbed/topic/229/?page=2#comment-8566
-    // see also https://os.mbed.com/users/no2chem/code/ClockControl//file/b5d3bd64d2dc/main.cpp/
-    // manual entry: Chapter 4: LPC17xx CLocking and power control
-
-    // current clock variables:
-    // Fin = 12000000; // 12 MHz XTAL
-    // M = (LPC_SC->PLL0CFG & 0xFFFF) + 1;
-    // N = (LPC_SC->PLL0CFG >> 16) + 1;
-    // CCLKDIV = LPC_SC->CCLKCFG + 1;
-    // Fcco = (2 * M * Fin) / N;
-    // CCLK = Fcco / CCLKDIV; // this should yield 96 MHz
-
-    LPC_SC->PLL0CON   = 0x00; // PLL0 Disable
-    LPC_SC->PLL0FEED  = 0xAA;
-    LPC_SC->PLL0FEED  = 0x55;
-
-    LPC_SC->CCLKCFG   = clkDiv - 1; // Select Clock Divisor
-    LPC_SC->PLL0CFG   = (((unsigned int)N - 1) << 16) | (M - 1); // configure PLL0
-    LPC_SC->PLL0FEED  = 0xAA;
-
-    LPC_SC->PLL0CON   = 0x01; // PLL0 Enable
-    LPC_SC->PLL0FEED  = 0xAA;
-    LPC_SC->PLL0FEED  = 0x55;
-
-    while (!(LPC_SC->PLL0STAT & (1<<26))) {} // Wait for PLOCK0
-
-    LPC_SC->PLL0CON   = 0x03; // PLL0 Enable & Connect
-    LPC_SC->PLL0FEED  = 0xAA;
-    LPC_SC->PLL0FEED  = 0x55;
-
-    while (!(LPC_SC->PLL0STAT & ((1<<25) | (1<<24)))) {} // Wait for PLLC0_STAT & PLLE0_STAT
-
-    SystemCoreClockUpdate(); // updates the SystemCoreClock global variable, should be 128 MHz now
-
-    // this is how SystemCoreClock is obtained:
-    // SystemCoreClock = (Fin * 2 /
-    //                              (((LPC_SC->PLL0STAT >> 16) & 0xFF) + 1) *
-    //                              ((LPC_SC->PLL0STAT & 0x7FFF) + 1) /
-    //                              ((LPC_SC->CCLKCFG & 0xFF) + 1));
-}
-
 int main()
 {
-    // JR3<CLOCK_PIN, DATA_PIN> jr3;
-    // JR3Port jr3(Port0, CLOCK_PIN, DATA_PIN);
-    JR3PortRegister<Port0, CLOCK_PIN, DATA_PIN> jr3;
+    // overclock CPU from 96 MHz to 128 MHz
+    // setSystemFrequency(3, 16, 1);
+
+    Jr3<Port0, CLOCK_PIN, DATA_PIN> jr3;
 
     const int STORAGE_SIZE = 500;
     int storage[STORAGE_SIZE];
@@ -62,15 +17,11 @@ int main()
 
     wait(5);
 
-    // overclock CPU from 96 MHz to 128 MHz
-    // setSystemFrequency(3, 16, 1);
-
-    Serial pc(USBTX, USBRX); // tx, rx
-    pc.printf("System clock = %d\n", SystemCoreClock);
+    printf("System clock = %d\n", SystemCoreClock);
 
     wait(5);
 
-    pc.printf("ready\n");
+    printf("ready\n");
 
     while (true)
     {
@@ -80,7 +31,7 @@ int main()
         {
             for (int i = 0; i < STORAGE_SIZE; i++)
             {
-                pc.printf("[%03d] [%d] 0x%04X\n", i + 1, (storage[i] & 0x000F0000) >> 16, storage[i] & 0x0000FFFF);
+                printf("[%03d] [%d] 0x%04X\n", i + 1, (storage[i] & 0x000F0000) >> 16, storage[i] & 0x0000FFFF);
             }
 
             storageIndex = 0;
