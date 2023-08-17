fbe7286caf57396cbcba4c27ad2f6817992b87d7
Bartek Łukawski <bwmn.peter@gmail.com>
2023-08-17 17:12:52 +0200

    Respond to PWM commands on secondary CAN channel

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
 #include "mbed.h"
 #include "Jr3.hpp"
 #include "Jr3Controller.hpp"
-
-constexpr auto JR3_PORT = Port0;
-constexpr auto CLOCK_PIN = p9;
-constexpr auto DATA_PIN = p10;
-
-constexpr auto CAN_RD_PIN = p30;
-constexpr auto CAN_TD_PIN = p29;
-constexpr auto CAN_BAUDRATE = 1000000;
-constexpr auto CAN_ID = 0x01;
+#include "Motor.h"
 
 enum jr3_can_ops : uint8_t
 {
@@ -42,45 +34,54 @@ int main()
 
     printf("ready\n");
 
-    CAN can(CAN_RD_PIN, CAN_TD_PIN, CAN_BAUDRATE);
-    can.filter(CAN_ID, 0x007F, CANStandard);
-    can.reset();
+    CAN can_jr3(MBED_CONF_APP_CAN_JR3_RD_PIN, MBED_CONF_APP_CAN_JR3_TD_PIN);
+    can_jr3.frequency(MBED_CONF_APP_CAN_JR3_BAUDRATE);
+    can_jr3.filter(MBED_CONF_APP_CAN_JR3_ID, 0x007F, CANStandard);
+    can_jr3.reset();
+
+    CAN can_lacquey(MBED_CONF_APP_CAN_LACQUEY_RD_PIN, MBED_CONF_APP_CAN_LACQUEY_TD_PIN);
+    can_jr3.frequency(MBED_CONF_APP_CAN_LACQUEY_BAUDRATE);
+    can_lacquey.filter(MBED_CONF_APP_CAN_LACQUEY_ID, 0x007F, CANStandard);
+    can_lacquey.reset();
 
     CANMessage msg_in;
     CANMessage msg_out_forces, msg_out_moments, msg_out_ack;
 
     msg_out_forces.len = msg_out_moments.len = 6;
-    msg_out_forces.id = (JR3_GET_FORCES << 7) + CAN_ID;
-    msg_out_moments.id = (JR3_GET_MOMENTS << 7) + CAN_ID;
+    msg_out_forces.id = (JR3_GET_FORCES << 7) + MBED_CONF_APP_CAN_JR3_ID;
+    msg_out_moments.id = (JR3_GET_MOMENTS << 7) + MBED_CONF_APP_CAN_JR3_ID;
 
     msg_out_ack.len = 0;
-    msg_out_ack.id = (JR3_ACK << 7) + CAN_ID;
+    msg_out_ack.id = (JR3_ACK << 7) + MBED_CONF_APP_CAN_JR3_ID;
 
-    using Jr3Reader = Jr3<JR3_PORT, CLOCK_PIN, DATA_PIN>;
+    using Jr3Reader = Jr3<MBED_CONF_APP_JR3_PORT, MBED_CONF_APP_JR3_CLOCK_PIN, MBED_CONF_APP_JR3_DATA_PIN>;
     Jr3Controller<Jr3Reader> controller;
 
+    Motor motor(MBED_CONF_APP_LACQUEY_PWM_PIN, MBED_CONF_APP_LACQUEY_FWD_PIN, MBED_CONF_APP_LACQUEY_REV_PIN);
+
     uint16_t data[6];
+    float pwm;
 
     while (true)
     {
-        if (can.read(msg_in))
+        if (can_jr3.read(msg_in))
         {
             switch ((msg_in.id & 0x0780) >> 7)
             {
             case JR3_START:
                 printf("received JR3 start command\n");
                 controller.start(parseCutOffFrequency(msg_in));
-                can.write(msg_out_ack);
+                can_jr3.write(msg_out_ack);
                 break;
             case JR3_STOP:
                 printf("received JR3 stop command\n");
                 controller.stop();
-                can.write(msg_out_ack);
+                can_jr3.write(msg_out_ack);
                 break;
             case JR3_ZERO_OFFS:
                 printf("received JR3 zero offsets command\n");
                 controller.calibrate();
-                can.write(msg_out_ack);
+                can_jr3.write(msg_out_ack);
                 break;
             case JR3_SET_FILTER:
                 printf("received JR3 set filter command\n");
@@ -92,10 +93,21 @@ int main()
         if (controller.acquire(data))
         {
             memcpy(msg_out_forces.data, data, 6); // fx, fy, fz
-            can.write(msg_out_forces);
+            can_jr3.write(msg_out_forces);
 
             memcpy(msg_out_moments.data, data + 3, 6); // mx, my, mz
-            can.write(msg_out_moments);
+            can_jr3.write(msg_out_moments);
+        }
+
+        if (can_lacquey.read(msg_in) && msg_in.len == sizeof(float))
+        {
+            memcpy(&pwm, msg_in.data, sizeof(float));
+            printf("PWM value received: %f\n", pwm);
+
+            if (pwm >= -100.0f && pwm <= 100.0f)
+            {
+                motor.speed(pwm / 100.0f);
+            }
         }
 
         wait_us(1); // this spins the CPU
