cc3fea131c3c7501a1a4e7b1154c6db9e07b2709
Bartek Łukawski <bwmn.peter@gmail.com>
2023-08-24 10:49:59 +0200

    Implement sync/async stream modes

--- main.cpp
+++ main.cpp
@@ -9,21 +9,23 @@
 
 enum can_ops : uint8_t
 {
-    JR3_START = 1,   // 0x080
-    JR3_STOP,        // 0x100
-    JR3_ZERO_OFFS,   // 0x180
-    JR3_SET_FILTER,  // 0x200
-    JR3_GET_FORCES,  // 0x280
-    JR3_GET_MOMENTS, // 0x300
-    JR3_ACK,         // 0x380
+    SYNC = 1,           // 0x080
+    JR3_START_SYNC,     // 0x100
+    JR3_START_ASYNC,    // 0x180
+    JR3_STOP,           // 0x200
+    JR3_ZERO_OFFS,      // 0x280
+    JR3_SET_FILTER,     // 0x300
+    JR3_GET_FORCES,     // 0x380
+    JR3_GET_MOMENTS,    // 0x400
+    JR3_ACK,            // 0x480
 #if MBED_CONF_APP_CAN_USE_GRIPPER || MBED_CONF_APP_CAN2_ENABLE
-    GRIPPER_PWM = 14 // 0x700
+    GRIPPER_PWM = 14    // 0x700
 #endif
 };
 
 float parseCutOffFrequency(const CANMessage & msg)
 {
-    if (msg.len == sizeof(float)) // 4
+    if (msg.len == 4)
     {
         float temp;
         memcpy(&temp, msg.data, sizeof(float));
@@ -51,6 +53,17 @@ void processGripperCommand(const CANMessage & msg, Motor & motor)
     }
 }
 #endif
+
+void sendData(CAN & can, CANMessage & msg_forces, CANMessage & msg_moments, uint16_t * data)
+{
+    memcpy(msg_forces.data, data, 6); // fx, fy, fz
+    can.write(msg_forces);
+
+    // there is no need to put a delay between these two writes since the NXP has a triple transmit buffer
+
+    memcpy(msg_moments.data, data + 3, 6); // mx, my, mz
+    can.write(msg_moments);
+}
 
 int main()
 {
@@ -85,53 +98,61 @@ int main()
     Motor motor(MBED_CONF_APP_LACQUEY_PWM_PIN, MBED_CONF_APP_LACQUEY_FWD_PIN, MBED_CONF_APP_LACQUEY_REV_PIN);
 #endif
 
-    uint16_t data[6];
-
     while (true)
     {
-        if (can.read(msg_in) && (msg_in.id & 0x07F) == MBED_CONF_APP_CAN_ID)
+        if (can.read(msg_in))
         {
-            switch ((msg_in.id & 0x0780) >> 7)
+            if (msg_in.id == SYNC << 7)
             {
-            case JR3_START:
-                printf("received JR3 start command\n");
-                controller.start(parseCutOffFrequency(msg_in));
-                can.write(msg_out_ack);
-                break;
-            case JR3_STOP:
-                printf("received JR3 stop command\n");
-                controller.stop();
-                can.write(msg_out_ack);
-                break;
-            case JR3_ZERO_OFFS:
-                printf("received JR3 zero offsets command\n");
-                controller.calibrate();
-                can.write(msg_out_ack);
-                break;
-            case JR3_SET_FILTER:
-                printf("received JR3 set filter command\n");
-                controller.setFilter(parseCutOffFrequency(msg_in));
-                break;
-#if MBED_CONF_APP_CAN_USE_GRIPPER
-            case GRIPPER_PWM:
-                processGripperCommand(msg_in, motor);
-                break;
-#endif
-            default:
-                printf("unsupported command: %d\n", (msg_in.id & 0x0780) >> 7);
-                break;
+                if (uint16_t data[6]; controller.acquire(data))
+                {
+                    sendData(can, msg_out_forces, msg_out_moments, data);
+                }
             }
-        }
-
-        if (controller.acquire(data))
-        {
-            // there is no need to put a delay between these two writes since the NXP has a triple transmit buffer
-
-            memcpy(msg_out_forces.data, data, 6); // fx, fy, fz
-            can.write(msg_out_forces);
-
-            memcpy(msg_out_moments.data, data + 3, 6); // mx, my, mz
-            can.write(msg_out_moments);
+            else if ((msg_in.id & 0x07F) == MBED_CONF_APP_CAN_ID)
+            {
+                switch ((msg_in.id & 0x0780) >> 7)
+                {
+                case JR3_START_SYNC:
+                    printf("received JR3 start command (synchronous)\n");
+                    controller.setFilter(parseCutOffFrequency(msg_in));
+                    controller.startSync();
+                    can.write(msg_out_ack);
+                    break;
+                case JR3_START_ASYNC:
+                    printf("received JR3 start command (asynchronous)\n");
+                    controller.setFilter(parseCutOffFrequency(msg_in));
+                    controller.startAsync([&can, &msg_out_forces, &msg_out_moments](uint16_t * data)
+                    {
+                        sendData(can, msg_out_forces, msg_out_moments, data);
+                    }, 0.0f); // TODO
+                    can.write(msg_out_ack);
+                    break;
+                case JR3_STOP:
+                    printf("received JR3 stop command\n");
+                    controller.stop();
+                    can.write(msg_out_ack);
+                    break;
+                case JR3_ZERO_OFFS:
+                    printf("received JR3 zero offsets command\n");
+                    controller.calibrate();
+                    can.write(msg_out_ack);
+                    break;
+                case JR3_SET_FILTER:
+                    printf("received JR3 set filter command\n");
+                    controller.setFilter(parseCutOffFrequency(msg_in));
+                    can.write(msg_out_ack);
+                    break;
+#if MBED_CONF_APP_CAN_USE_GRIPPER
+                case GRIPPER_PWM:
+                    processGripperCommand(msg_in, motor);
+                    break;
+#endif
+                default:
+                    printf("unsupported command: %d\n", (msg_in.id & 0x0780) >> 7);
+                    break;
+                }
+            }
         }
 
 #if MBED_CONF_APP_CAN2_ENABLE
