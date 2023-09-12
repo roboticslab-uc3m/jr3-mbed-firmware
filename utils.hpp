74e98c6a4562c6e1b2750d743beb2b889b5bc4ae
Bartek Łukawski <bwmn.peter@gmail.com>
2023-09-12 11:23:42 +0200

    Use fixed-point operations on converted floats

--- utils.hpp
+++ utils.hpp
@@ -1,17 +1,26 @@
 #ifndef __UTILS_HPP__
 #define __UTILS_HPP__
 
-#include "mbed.h"
+#include "cstdint"
+#include "fixed_class.h"
 
-float jr3FloatToIEEE754(int8_t exponent, uint16_t mantissa);
+constexpr int JR3_PRECISION = 15;
+constexpr int FIXED_PRECISION = 31;
 
-inline float jr3FixedToIEEE754(uint16_t fixed)
+using fixed_t = fixedpoint::fixed_point<FIXED_PRECISION>; // (-1, 1]
+
+inline fixed_t jr3ToFixedPoint(uint16_t mantissa, int8_t exponent = 0x00)
 {
+    fixed_t f;
     // beware of integral promotion! https://stackoverflow.com/a/30474166
-    int8_t exponent = __CLZ((fixed >> 15 ? ~fixed : fixed) & 0x0000FFFF) - 17;
-    return jr3FloatToIEEE754(-exponent, fixed << exponent);
+    f.intValue = ((~mantissa + 1U) & (mantissa >> JR3_PRECISION ? 0x0000FFFF : 0xFFFFFFFF)) << (FIXED_PRECISION - JR3_PRECISION + exponent);
+    return f;
 }
 
-uint16_t jr3FixedFromIEEE754(float f);
+inline uint16_t jr3FromFixedPoint(fixed_t f)
+{
+    uint16_t temp = (~f.intValue + 1U) >> (FIXED_PRECISION - JR3_PRECISION);
+    return temp;
+}
 
 #endif // __UTILS_HPP__
